ada546322105c5a9e620748bb8b792faf104026a
Noah Tah <noahtah21@gmail.com>
2025-04-25 10:04:13 -0500

    batman

---
+++ scripting-final-assignment-noah-tah.py
@@ -0,0 +1,13 @@
+'''
+Noah Tah
+4/25/2025
+Scripting Final Assignment
+Programming II
+Evan Vaverka
+Northwestern Oklahoma State University
+Assignment Description:
+	Three Little Pigs
+'''
+
+def main (void): 
+	print("Welcome to the Three Little Pigs game!")
