d119287b352f4f71d5103311c3b3352057240945
Noah Tah <noahtah21@gmail.com>
2025-04-25 14:03:14 -0500

    jeffery to jeffrey lol

--- scripting-final-assignment-noah-tah.py
+++ scripting-final-assignment-noah-tah.py
@@ -8,8 +8,14 @@ Northwestern Oklahoma State University
 Assignment Description:
 	Three Little Pigs
 
-	Stolen Wi-Fi – The pigs have been mooching off the wolf’s Wi-Fi, and he’s sick of their Minecraft server lagging his Netflix.
+	Stolen Wi-Fi – The pigs have been mooching off the wolf's Wi-Fi, and he's sick of their Minecraft server lagging his Netflix.
 '''
+
+DestroyedComputers = {
+	"jeffrey": False,
+	"billy": False,
+	"theodore": False
+}
 
 def main (void): 
 	print("Welcome to the Three Little Pigs game!")
@@ -35,7 +41,7 @@ def main (void):
 	choice = input("Enter the number of the pig you want to catch: ")
 
 	if choice == "1":
-		jefferyPig()
+		jeffreyPig()
 	elif choice == "2":
 		billyPig()
 	elif choice == "3":
@@ -45,43 +51,67 @@ def main (void):
 		main()
 
 
-def jefferyPig():
-	print("You head to Jeffery Pig's aapartment.")
+def jeffreyPig():
+	print("You head to Jeffrey Pig's apartment.")
 	print("You smell a skunk nearby, and you can hear an intense amount of clicking, they must be playing Minecraft.")
-	print("You knock on the door, and Jeffery Pig opens it.")
+	print("You knock on the door, and Jeffrey Pig opens it.")
 	print("He looks at you and says, 'What do you want? I'm trying to play Minecraft here!'")
 	print('You say, "Your minecraft server is lagging grandma wolf\'s Netflix and we can\'t watch anime!"')
 	print("My server? It's not even my server, it's Billy's server!")
 
 	print('You say, "Are you LYING to me?"')
-	print("Jeffery Pig looks at you and says, 'No, I'm not lying! I swear!'")
+	print("Jeffrey Pig looks at you and says, 'No, I'm not lying! I swear!'")
 
-	print("There is a chance that Jeffery Pig is lying to you.")
+	print("There is a chance that Jeffrey Pig is lying to you.")
 	print("You can either:")
 	print("1. Decide he's lying and destroy his computer")
 	print("2. Decide he's telling the truth and go get Billy Pig")
 
 	choice = input("Enter the number of your choice: ")
 	if choice == "1":
-		desroyJefferysComputer()
+		destroyJeffreysComputer()
 	elif choice == "2":
 		billyPig()
 	else:
 		print("Invalid choice. Please enter 1 or 2.")
-		jefferyPig()
+		jeffreyPig()
 
-def desroyJefferysComputer():
-	print("You decide that Jeffery Pig is lying to you.")
+def destroyJeffreysComputer():
+	print("You decide that Jeffrey Pig is lying to you.")
 	print("You destroy his computer, and he starts crying.")
 	print("You feel bad for him, but you know you have to do it so grandma can learn that anime is totally awesome.")
 
-	print('Jeffery pig says, "You\'re a monster! You didn\'t even stop the server, you just destroyed my computer!"')
+	print('Jeffrey Pig says, "You\'re a monster! You didn\'t even stop the server, you just destroyed my computer!"')
 	print('"See? Look!" he holds up his phone and shows you a livestream of Billy Pig playing Minecraft on the server.')
 	print('"I told you it wasn\'t my server!"')
-	print("You feel bad for Jeffery Pig, but you know you have to do it.")
+	print("You feel bad for Jeffery Pig, but you know his parents are loaded anyways.")
 	print("You head over to Billy Pig's apartment.")
 
 	billyPig()
+
+def billyPig():
+	print("You head over to Billy Pig's apartment.")
+	print("You knock on the door, and Billy Pig opens it.")
+	print("He looks at you and says, 'What do you want? I'm trying to play Minecraft here!'")
+	print('You say, "Your minecraft server is lagging grandma wolf\'s Netflix and we can\'t watch anime!"')
+	print("My server? It's not even my server, it's Theodore's server!")
+
+	print('You say, "Are you LYING to me?"')
+	print("Billy Pig looks at you and says, 'No, I'm not lying! I swear!'")
+
+	print("There is a chance that Billy Pig is lying to you.")
+	print("You can either:")
+	print("1. Decide he's lying and destroy his computer")
+	print("2. Decide he's telling the truth and go get Theodore Pig")
+
+	choice = input("Enter the number of your choice: ")
+	if choice == "1":
+		destroyBillysComputer()
+	elif choice == "2":
+		theodorePig()
+	else:
+		print("Invalid choice. Please enter 1 or 2.")
+		billyPig()
 
 	
 
